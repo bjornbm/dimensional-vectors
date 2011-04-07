c0e72664f2387d5502b82314fe32e7854239b69e
Bjorn Buckwalter <bjorn@buckwalter.se>
2011-04-07 22:50:40 +0800

    Clean up AD a little. What to do with List class??

--- AD.hs
+++ AD.hs
@@ -1,8 +1,9 @@
--- This module provides forwards automatic differentiation for quantities.
+-- | This module provides automatic differentiation for Quantities.
 
-{-# OPTIONS_GHC -fglasgow-exts #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE Rank2Types #-}
 
-module AD (diff, Lift, lift) where
+module AD (diff, Lift (lift)) where
 
 import Numeric.Units.Dimensional (Dimensional (Dimensional), Quantity, Div)
 import Numeric.AD (AD, Mode)
@@ -12,13 +13,19 @@ import qualified Numeric.AD (diff, lift)
 undim :: Dimensional v d a -> a
 undim (Dimensional a) = a
 
+-- | @diff f x@ computes the derivative of the function @f(x)@ for the
+-- given value of @x@.
 diff :: (Num a, Div d2 d1 d3)
      => (forall tag. Mode tag => Quantity d1 (AD tag a) -> Quantity d2 (AD tag a))
      -> Quantity d1 a -> Quantity d3 a
 diff f = Dimensional . Numeric.AD.diff (undim . f . Dimensional) . undim
 
+-- | Class to provide 'Numeric.AD.lift'ing of constant data structures
+-- (data structures with numeric constants used in a differentiated
+-- function).
+class Lift w where
+  -- | Embed a constant data structure.
+  lift :: (Num a, Mode t) => w a -> w (t a)
 
-class Lift w where lift :: (Num a, Mode t) => w a -> w (t a)
 instance Lift (Dimensional v d)
-  --where lift (Dimensional x) = Dimensional (Numeric.AD.lift x)
   where lift = Dimensional . Numeric.AD.lift . undim
