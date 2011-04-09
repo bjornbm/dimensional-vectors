68619d384f4df283e2a21e0c1e153087ddb70281
Bjorn Buckwalter <bjorn@buckwalter.se>
2011-04-10 07:42:07 +0800

    Don't export ListVec and MatVec through LinearAlgebra.
    Also added QUESTIONS and TODO sections for stuff I need to take a
    decision on, and do.

--- Numeric/Units/Dimensional/LinearAlgebra.hs
+++ Numeric/Units/Dimensional/LinearAlgebra.hs
@@ -5,7 +5,19 @@ module Numeric.Units.Dimensional.LinearAlgebra
   , (:*.)
   ) where
 
-import Numeric.Units.Dimensional.LinearAlgebra.Vector hiding (vMap, vZipWith)
-import Numeric.Units.Dimensional.LinearAlgebra.Matrix
+import Numeric.Units.Dimensional.LinearAlgebra.Vector hiding (ListVec, vMap, vZipWith)
+import Numeric.Units.Dimensional.LinearAlgebra.Matrix hiding (ListMat)
 import Numeric.Units.Dimensional.LinearAlgebra.Operators
 import Numeric.Units.Dimensional.LinearAlgebra.HListExtras ((:*.))
+
+{-
+QUESTIONS:
+  - Should classes have functions or not?
+  - Should I define classes for each function? Would it make contraints
+more clear or less clear? Would it hide HList type classes?
+  - Should I prevent empty vectors/matrices from ever being constructed? Could they cause issues when I change underlying implementation?
+  - Want to change to an efficient underlying implementation, but which one? Would prefer a pure Haskell one with minimal dependencies. Would it be possible to offer several by parameterization (the a type variable)?
+
+TODO:
+  - Convert from .lhs / write proper haddocks.
+-}
