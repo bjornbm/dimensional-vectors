76b3be9485102237a531ecca7b52e256e3bcd10f
Bjorn Buckwalter <bjorn@buckwalter.se>
2014-09-24 20:27:21 +0200

    Revive arbitary VApply.

--- VApply.hs
+++ VApply.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -64,4 +65,23 @@ instance VApplyVC (Vec ds1 a -> Vec ds2 a) ds1 a where
   vApplyV f = f
 
 
+-- ==============================
 
+-- Vector to anything.
+class VApplyC f ds a where
+  type VApply f ds a :: k  -- Has to be * for vApply to make sense.
+  -- | Apply a function with arbitrary return type to a vector.
+    --
+    -- >>> vApply Id v == v
+    -- True
+    -- >>> vApply Sum vh1 == vSum vh1
+    -- True
+  vApply :: f -> Vec ds a -> VApply f ds a
+
+instance VApplyC Id ds a where
+  type VApply Id ds a = Vec ds a
+  vApply Id = id
+
+instance Num a => VApplyC Sum ds a where
+  type VApply Sum ds a = Quantity (Homo ds) a
+  vApply Sum = vSum
